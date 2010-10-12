a3be7c722421ccb7a92bba6f6b281cc1acf9a710
luizcpo <luizcpo@90bf4469-9d35-2d56-c726-e91037860998>
2010-10-12 17:48:13 +0000

    - Criação das classes PersistenciaTeste utilizando JUnit - Geração de testes de para os métodos cadastro e save da classe PersistenciaClientes

---
+++ trunk/tp1-POO/src/Persistencia/PersistenciaEstoqueTest.java
@@ -0,0 +1,44 @@
+package Persistencia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PersistenciaEstoqueTest {
+
+	@Test
+	public void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveCompra() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSavePedido() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchItemInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchItemString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCadastro() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOverview() {
+		fail("Not yet implemented");
+	}
+
+}
