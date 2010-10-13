14dc6a3dcaecbf1206f9d103557497e75e63b27b
luizcpo <luizcpo@90bf4469-9d35-2d56-c726-e91037860998>
2010-10-13 23:21:41 +0000

    - Correção da classe EstoqueTest

--- tp1-POO/test/estoque/EstoqueTest.java
+++ tp1-POO/test/estoque/EstoqueTest.java
@@ -8,9 +8,9 @@ public class EstoqueTest {
 	
 	@Test
 	public void testCalculaPrecoMedioPonderado() {
-		Item itemTest = new Item ("Camisa",1200,(float)1,(float)20,10);
-		ItemCompra itemCompraTest = new ItemCompra (itemTest,(float) 1.10,10);
-		assertEquals(1.05, Estoque.calculaPrecoMedioPonderado(itemCompraTest),0.1);
+		Item itemTest = new Item ("Camisa",1200,(float)1.05,(float)20,7);
+		ItemCompra itemCompraTest = new ItemCompra (itemTest,(float)1.2,10);
+		assertEquals(1.138235294, Estoque.calculaPrecoMedioPonderado(itemCompraTest),0.1);
 	}
 
 }
