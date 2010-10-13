aa23e9adaf8d0013f09fb16fb5e1e60c5d5f7be4
isacsandin@gmail.com <isacsandin@gmail.com@90bf4469-9d35-2d56-c726-e91037860998>
2010-10-13 13:56:37 +0000

    adicionando classes de teste

--- trunk/tp1-POO/src/cui/Main.java
+++ trunk/tp1-POO/src/cui/Main.java
@@ -9,7 +9,6 @@ public class Main {
 	 * 
 	 */
 	public static void main(String[] args) throws IOException {
-		System.out.println(Console.date2str(Console.str2date("26/09/1988","dd/MM/yyyy"),"dd/MM/yyyy"));
 		Menu.principal();
 	}
 	
