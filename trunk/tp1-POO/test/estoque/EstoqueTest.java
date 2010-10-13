75061c11779a96e2faf2969a2eff62c44029a3fa
amandaufsj@gmail.com <amandaufsj@gmail.com@90bf4469-9d35-2d56-c726-e91037860998>
2010-10-13 23:08:54 +0000

    correacao estoqueTest

--- trunk/tp1-POO/test/estoque/EstoqueTest.java
+++ trunk/tp1-POO/test/estoque/EstoqueTest.java
@@ -6,13 +6,11 @@ import org.junit.Test;
 
 public class EstoqueTest {
 	
-	@SuppressWarnings("deprecation")
 	@Test
 	public void testCalculaPrecoMedioPonderado() {
 		Item itemTest = new Item ("Camisa",1200,(float)1,(float)20,10);
 		ItemCompra itemCompraTest = new ItemCompra (itemTest,(float) 1.10,10);
-		float result = (float) 1.05;
-		assertEquals(result, Estoque.calculaPrecoMedioPonderado(itemCompraTest));
+		assertEquals(1.05, Estoque.calculaPrecoMedioPonderado(itemCompraTest),0.1);
 	}
 
 }
